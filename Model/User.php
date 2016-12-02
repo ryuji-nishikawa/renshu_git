ea82a4597a99f87d350f10f16b089959471e5acc
Ryuji Nishikawa <ryuji_nishikawa@tempo.ocn.ne.jp>
2016-12-02 12:55:25 +0900

    フォルダごとまとめてコミットした

---
+++ Model/User.php
@@ -0,0 +1,26 @@
+<?php
+class User extends AppModel{
+	public $validate = array(
+		'username' => array(
+			array(
+				'rule' => 'notBlank',
+				'message' => '名前は必ず入力してください',
+			),
+			array(
+				'rule' => array('maxLength',32),
+				'message' => '名前が長すぎます',
+			),
+			array(
+				'rule' => 'alphaNumeric',
+				'message' => '名前は半角英数字で入力してください',
+			),
+		),
+		'description' => array(
+			array(
+				'rule' => array('maxLength',200),
+				'message' => '備考が長すぎます',
+			),
+		),
+	);
+}
+?>
