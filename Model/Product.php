4e2fa800c11a05cb4419be4489668c2ffcebe1ad
Ryuji Nishikawa <ryuji_nishikawa@tempo.ocn.ne.jp>
2016-12-12 18:39:20 +0900

    saleは6回目、productは初めて変更

--- Model/Product.php
+++ Model/Product.php
@@ -1,10 +1,11 @@
 <?php
-	App::uses('AppModel', 'Model');
+	App::uses('AppModel', 'これはproductの変更1回目');
 	class Product extends AppModel{
 		private function rara(){
 			$this->rara() = 2;
 		}
 	}
+
 	/* git pushの練習 */
 	/* gitのコンフリクトの練習 */
 ?>
