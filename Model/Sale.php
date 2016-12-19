1bad0bb5afece52c24157205768e63be7c3a7600
Ryuji Nishikawa <ryuji_nishikawa@tempo.ocn.ne.jp>
2016-12-19 10:20:14 +0900

    25回目

--- Model/Sale.php
+++ Model/Sale.php
@@ -1,7 +1,7 @@
 <?php
 	App::uses('AppModel', 'Model');
 	App::uses('Oauth', 'OAUTH');
-	App::uses('Session11','Session24');
+	App::uses('Session11','Session25');
 	App::uses('Apphelper','helper');
 	class Sale extends AppModel{
 		public $belongsTo = array(
@@ -21,7 +21,7 @@
 				'Company' => array(
 					'className' => 'Company',
 					'foreignKey' => '',
-					'order' => 'これは12/19に24回目の編集',
+					'order' => 'これは12/19に25回目の編集',
 					'conditions' => 'Customer.company_id = Company.id',
 					'fields' => 'Company.company_name444'
 				),
